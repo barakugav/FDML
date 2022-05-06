6d63278480c0c2aab3ff6629282d70b970a31615
Michael Bilevich <mickelbil84@gmail.com>
2022-05-06 12:02:10 +0300

    Baseline of voxel manifold intersection

--- meshing/cgal_include.h
+++ meshing/cgal_include.h
@@ -1,6 +1,8 @@
 #ifndef CGAL_INCLUDE_H_
 #define CGAL_INCLUDE_H_
 
+#include <boost/optional/optional_io.hpp>
+
 #include <CGAL/Complex_2_in_triangulation_3.h>
 #include <CGAL/IO/facets_in_complex_2_to_triangle_mesh.h>
 #include <CGAL/Implicit_surface_3.h>
@@ -8,6 +10,12 @@
 #include <CGAL/Surface_mesh_default_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
 #include <CGAL/squared_distance_2.h>
+
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_traits.h>
+#include <CGAL/AABB_face_graph_triangle_primitive.h>
+#include <CGAL/Polygon_mesh_processing/compute_normal.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
 
 #include <CGAL/Arr_non_caching_segment_traits_2.h>
 #include <CGAL/Arr_trapezoid_ric_point_location.h>
@@ -24,6 +32,9 @@ typedef GT::FT FT;
 typedef FT (*Function)(Point_3);
 typedef CGAL::Implicit_surface_3<GT, Function> Surface_3;
 typedef CGAL::Surface_mesh<Point_3> Surface_mesh;
+typedef Surface_mesh::Vertex_index Vertex_descriptor;
+typedef Surface_mesh::Face_index Face_descriptor;
+
 
 typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
 typedef Kernel::FT Number_type;
@@ -35,6 +46,11 @@ typedef Arrangement::Vertex_handle Vertex_handle;
 typedef Arrangement::Halfedge_handle Halfedge_handle;
 typedef Arrangement::Face_handle Face_handle;
 typedef CGAL::Arr_trapezoid_ric_point_location<Arrangement> Trap_pl;
+
+typedef CGAL::AABB_face_graph_triangle_primitive<Surface_mesh> Primitive;
+typedef CGAL::AABB_traits<Kernel, Primitive> AABB_Traits;
+typedef CGAL::AABB_tree<AABB_Traits> Tree;
+typedef CGAL::Bbox_3 Bbox_3;
 
 #define INFTY 1000.0
 
--- meshing/meshing.cpp
+++ meshing/meshing.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "cgal_include.h"
+#include "manifold_intersection.h"
 #include "meshing_options.h"
 #include "read_input.h"
 #include "shoot_ray.h"
@@ -19,12 +20,44 @@ int main(int argc, char** argv) {
   load_poly_to_arrangement(mo.filename, &arr);
   Trap_pl pl(arr);
 
-  Surface_mesh sm;
-  single_measurement(sm, arr, pl, mo.d1, Point_3(mo.sphere_x, mo.sphere_y, mo.sphere_z), mo.sphere_r, mo.angle_bound,
-                     mo.radius_bound, mo.distance_bound);
+  if (mo.single_measurement) {
+    Surface_mesh sm;
+    single_measurement(sm, arr, pl, mo.d1, Point_3(mo.sphere_x, mo.sphere_y, mo.sphere_z), mo.sphere_r, mo.angle_bound,
+                       mo.radius_bound, mo.distance_bound);
 
-  std::ofstream out(mo.out_filename);
-  out << sm << std::endl;
+    std::ofstream out(mo.out_filename);
+    out << sm << std::endl;
+  } else {
+    std::cout << "Unsupported mode - DEBUG code only." << std::endl;
+
+    std::function<Point_3(Point_3)> rotate_alpha = [&](Point_3 p) {
+      FT theta = p.z() + mo.alpha / (2 * M_PI);
+      if (theta > 1.0)
+        theta -= 1.0;
+      return Point_3(p.x(), p.y(), theta);
+    };
+
+    Surface_mesh m_d1, m_d2;
+    std::cout << "Computing M_d1..." << std::endl;
+    single_measurement(m_d1, arr, pl, mo.d1, Point_3(mo.sphere_x, mo.sphere_y, mo.sphere_z), mo.sphere_r,
+                       mo.angle_bound, mo.radius_bound, mo.distance_bound);
+    std::cout << "Done computing M_d1." << std::endl;
+    std::cout << "Computing M_d2..." << std::endl;
+    single_measurement(m_d2, arr, pl, mo.d2, Point_3(mo.sphere_x, mo.sphere_y, mo.sphere_z), mo.sphere_r,
+                       mo.angle_bound, mo.radius_bound, mo.distance_bound, rotate_alpha);
+    std::cout << "Done computing M_d2." << std::endl;
+
+    Surface_mesh v_square;
+    DeltaCube initial_cube(Point_3(-3,-3,-3), Point_3(3,3,3));
+    manifold_intersection(m_d1, m_d2, v_square, initial_cube, 0.005, 0.01);
+
+    std::ofstream out(mo.out_filename);
+    out << v_square << std::endl;
+    std::ofstream out1("out/m_d1.off");
+    out1 << m_d1 << std::endl;
+    std::ofstream out2("out/m_d2.off");
+    out2 << m_d2 << std::endl;
+  }
 
   return 0;
 }
