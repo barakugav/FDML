a45d49da3b035c0f7fcf7f0b1fe53e61566c02cc
Efi Fogel <efifogel@gmail.com>
2022-05-12 16:28:19 +0300

    Updated

--- doc/two_measurements_data/sol_orthogonal.m
+++ doc/two_measurements_data/sol_orthogonal.m
@@ -45,13 +45,13 @@
 
 syms d1 d2 k m1 m2 x y
 eq = 4 * ((x + m1 * y)^2 - (1 + m1^2) * (x^2 + y^2 - d1^2)) / (1 + m1^2)^2 * ((x + m2 * y)^2 - (1 + m2^2) * (x^2 + y^2 - d2^2)) / (1 + m2^2)^2 * (x + m1 * y)^2 * (x + m2 * y)^2 == (((x + m1 * y) / (1 + m1^2) * (x + m1 * y) + (x + m2 * y) / (1 + m2^2) * (x + m2 * y) - x^2 - y^2 + k)^2 - ((x + m1 * y)^2 - (1 + m1^2) * (x^2 + y^2 - d1^2)) / (1 + m1^2)^2 * (x + m1 * y)^2 - ((x + m2 * y)^2 - (1 + m2^2) * (x^2 + y^2 - d2^2)) / (1 + m2^2)^2 * (x + m2 * y)^2)^2
-sol_o = reduceRedundancies([eq, m1 * m2 == -1], [x, y, m2])
+sol_o = reduceRedundancies([eq, m1 * m2 == -1], [x, y, m1])
 
-fid_o = fopen('sol_o.txt', 'wt');
-fprintf(fid_o, '%s\n', char(sol_o));
-fclose(fid_o);
+fid_o = fopen('sol_o2.txt', 'wt')
+fprintf(fid_o, '%s\n', char(sol_o))
+fclose(fid_o)
 
-sol_oa = subs(sol_o, [m1], [0])
-fid_oa = fopen('sol_oa.txt', 'wt');
-fprintf(fid_oa, '%s\n', char(sol_oa));
-fclose(fid_oa);
+%% sol_oa = subs(sol_o, [m1], [0])
+%% fid_oa = fopen('sol_o.txt', 'wt')
+%% fprintf(fid_oa, '%s\n', char(sol_oa))
+%% fclose(fid_oa)
