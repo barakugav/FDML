9df7e4912c100afe284d7ffe091b1c62a8ebeda8
Efi Fogel <efifogel@gmail.com>
2022-05-15 20:21:42 +0300

    updated

--- doc/two_measurements_data/compute_coeffs.m
+++ doc/two_measurements_data/compute_coeffs.m
@@ -10,5 +10,9 @@ function [sol_coeffs] = compute_coeffs(p1, p2, p3, p4, p5, p6, p7, p8, p9, hint)
   eq8 = B1*D2 + B2*D1 == p8           	% y^2
   eq9 = D1*D2 == p9                   	% free
 
-  sol_coeffs = solve([eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9, hint], [A1, B1, C1, D1, A2, B2, C2, D2], 'Real', true)
+  if nargin > 9
+    sol_coeffs = solve([eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9, hint], [A1, B1, C1, D1, A2, B2, C2, D2], 'Real', true)
+  else
+    sol_coeffs = solve([eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9], [A1, B1, C1, D1, A2, B2, C2, D2], 'Real', true)
+  end
 end
--- doc/two_measurements_data/solution.m
+++ doc/two_measurements_data/solution.m
@@ -62,17 +62,45 @@ fid = fopen('sol.txt', 'wt')
 fprintf(fid, '%s\n', char(sol))
 fclose(fid)
 
+%%%%%%%% Solve for the x-axis aligned case
 sol_a = subs(sol, [m1], [0])
 fid_a = fopen('sol_a.txt', 'wt')
 fprintf(fid_a, '%s\n', char(sol_a))
 fclose(fid_a)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = compute_terms(sol_a)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = negate_terms(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+syms A1 B1 C1 D1 A2 B2 C2 D2 real
+eq10 = A1 == d1^2*m2^2
+eq14 = A2 == d1^2*m2^2
+sol_c_a = compute_coeffs(p1, p2, p3, p4, p5, p6, p7, p8, p9, eq10)
+fid_c_a = fopen('sol_c_a.txt', 'wt')
+print_coeffs(fid_c_a, sol_c_a)
+fclose(fid_c_a)
 
+%%%%%%%% Solve for the orthogonal case
 sol_o = reduceRedundancies([eq, m1 * m2 == -1], [x, y, m2])
 fid_o = fopen('sol_o.txt', 'wt')%
 fprintf(fid_o, '%s\n', char(sol_o))
 fclose(fid_o)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = compute_terms(sol_o)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = negate_terms(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+syms A1 B1 C1 D1 A2 B2 C2 D2 real
+eq17 = D2 == k^2*((-1/m2) - m2)
+sol_c_o = compute_coeffs(p1, p2, p3, p4, p5, p6, p7, p8, p9, eq17)
+fid_c_o = fopen('sol_c_o.txt', 'wt')
+print_coeffs(fid_c_o, sol_c_o)
+fclose(fid_c_o)
 
+%%%%%%%% Solve for the simplest case, namely, orthogonal and axis aligned
 sol_oa = subs(sol_o, [m1], [0])
 fid_oa = fopen('sol_oa.txt', 'wt')
 fprintf(fid_oa, '%s\n', char(sol_oa))
 fclose(fid_oa)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = compute_terms(sol_oa)
+[p1, p2, p3, p4, p5, p6, p7, p8, p9] = negate_terms(p1, p2, p3, p4, p5, p6, p7, p8, p9)
+syms A1 B1 C1 D1 A2 B2 C2 D2 real
+eq13 = D1 == -k^2
+sol_c_oa = compute_coeffs(p1, p2, p3, p4, p5, p6, p7, p8, p9, eq13)
+fid_c_oa = fopen('sol_c_oa.txt', 'wt')
+print_coeffs(fid_c_oa, sol_c_oa)
+fclose(fid_c_oa)
