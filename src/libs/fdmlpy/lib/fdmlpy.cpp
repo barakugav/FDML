c60e7d8ac02f58583aa4e0effe3af9abb8387970
Efi Fogel <efifogel@gmail.com>
2022-05-18 18:31:34 +0300

    Added bindings for query2

--- src/libs/fdmlpy/lib/fdmlpy.cpp
+++ src/libs/fdmlpy/lib/fdmlpy.cpp
@@ -48,6 +48,25 @@ bp::list query1(const FDML::Locator& locator, const FDML::Kernel::FT& d) {
   return lst;
 }
 
+bp::list query2(const FDML::Locator& locator,
+                const FDML::Kernel::FT& d1, const FDML::Kernel::FT& d2) {
+  std::vector<FDML::Locator::Res2d> pls = locator.query(d1, d2);
+  bp::list lst;
+  /* TODO return to python the measured edge along with the possible position polygon */
+  for (auto pl : pls) {
+    bp::tuple edge1 = bp::make_tuple(pl.edge1.first, pl.edge1.second);
+    bp::tuple edge2 = bp::make_tuple(pl.edge2.first, pl.edge2.second);
+    bp::list res;
+    res.append(edge1);
+    res.append(edge2);
+    bp::list polyline;
+    for (const auto& e : pl.pos) polyline.append(e);
+    res.append(polyline);
+    lst.append(res);
+  }
+  return lst;
+}
+
 BOOST_PYTHON_MODULE(fdmlpy) {
   typedef FDML::Kernel::FT      FT;
   typedef FDML::Polygon         Polygon;
@@ -73,6 +92,7 @@ BOOST_PYTHON_MODULE(fdmlpy) {
   bp::class_<FDML::Locator>("Locator", bp::init<>())
     .def("init", &Locator::init)
     .def("query1", &query1)
+    .def("query2", &query2)
     // .def<Query1>("query1", &Locator::query)
     // .def<Query2>("query2", &Locator::query)
     ;
