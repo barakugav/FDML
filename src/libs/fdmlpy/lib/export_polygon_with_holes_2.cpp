d4dceee9b39950ba04694f1ba28f7ba857e27b07
Efi Fogel <efifogel@gmail.com>
2022-08-22 11:34:40 +0300

    Resolved overload of Polygon_with_holes_2::add_hole() due to new addition to CGAL

--- src/libs/fdmlpy/lib/export_polygon_with_holes_2.cpp
+++ src/libs/fdmlpy/lib/export_polygon_with_holes_2.cpp
@@ -36,20 +36,22 @@ FDML::Polygon& outer_boundary(FDML::Polygon_with_holes& p) {
 } // namespace pol2
 
 void export_polygon_with_holes_2() {
-    typedef FDML::Polygon Polygon;
-    typedef FDML::Polygon_with_holes Polygon_with_holes;
+  using Pgn = FDML::Polygon ;
+  using Pwh = FDML::Polygon_with_holes;
 
-    bp::class_<Polygon_with_holes>("Polygon_with_holes_2")
-        .def(bp::init<Polygon&>())
-        .def("__init__", make_constructor(&pol2::init_polygon_with_holes_2))
-        .def("is_unbounded", &Polygon_with_holes::is_unbounded)
-        .def("outer_boundary", &pol2::outer_boundary, bp::return_internal_reference<>())
-        .def("holes", bp::range<bp::return_internal_reference<>>(&pol2::holes_begin, &pol2::holes_end))
-        .def("number_of_holes", &Polygon_with_holes::number_of_holes)
-        .def("bbox", &Polygon_with_holes::bbox)
-        .def("add_hole", &Polygon_with_holes::add_hole)
-        .def(bp::self_ns::str(bp::self_ns::self))
-        .def(bp::self_ns::repr(bp::self_ns::self))
-        .def(bp::self == bp::self)
-        .def(bp::self != bp::self);
+  bp::class_<Pwh>("Polygon_with_holes_2")
+    .def(bp::init<Pgn&>())
+    .def("__init__", make_constructor(&pol2::init_polygon_with_holes_2))
+    .def("is_unbounded", &Pwh::is_unbounded)
+    .def("outer_boundary", &pol2::outer_boundary,
+         bp::return_internal_reference<>())
+    .def("holes", bp::range<bp::return_internal_reference<>>(&pol2::holes_begin,
+                                                             &pol2::holes_end))
+    .def("number_of_holes", &Pwh::number_of_holes)
+    .def("bbox", &Pwh::bbox)
+    .def("add_hole", static_cast<void(Pwh::*)(const Pgn&)>(&Pwh::add_hole))
+    .def(bp::self_ns::str(bp::self_ns::self))
+    .def(bp::self_ns::repr(bp::self_ns::self))
+    .def(bp::self == bp::self)
+    .def(bp::self != bp::self);
 }
