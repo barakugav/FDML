45da0f812627449d0808043db9d5c08d6aab55b8
Barak Ugav <barakugav@gmail.com>
2023-05-08 18:07:03 +0300

    Bindings: remove old boost::py sources

--- fdmlpy/include/fdmlpy/Hash_rational_point.hpp
+++ fdmlpy/include/fdmlpy/Hash_rational_point.hpp
@@ -54,34 +54,6 @@ size_t hash_rational_point(Point& p) {
   return Hash_rational_point<b>::hash_rational_point(p);
 };
 
-// #if (CGALPY_KERNEL == CGALPY_KERNEL_EPEC) || \
-//   (CGALPY_KERNEL == CGALPY_KERNEL_EPEC_WITH_SQRT) || \
-//   (CGALPY_KERNEL == CGALPY_KERNEL_FILTERED_SIMPLE_CARTESIAN_LAZY_GMPQ) || \
-//   (CGALPY_KERNEL_D == CGALPY_KERNEL_D_EPEC_D) || \
-//   (CGALPY_KERNEL_D == CGALPY_KERNEL_D_CARTESIAN_D_LAZY_GMPQ)
-// template <typename Point>
-// size_t hash_rational_point(Point& p) {
-//   size_t seed = 0;
-//   for (auto c = p.cartesian_begin(); c != p.cartesian_end(); ++c) {
-//     auto q = (*c).exact();
-//     auto simplify =
-//       typename CGAL::Algebraic_structure_traits<decltype(q)>::Simplify();
-//     CGAL::Rational_traits<decltype(q)> traits;
-//     simplify(q);
-//     boost::hash_combine(seed, CGAL::to_double(traits.numerator(q)));
-//     boost::hash_combine(seed, CGAL::to_double(traits.denominator(q)));
-//   }
-//   return seed;
-// }
-// #endif
 
-// #if (CGALPY_KERNEL == CGALPY_KERNEL_EPIC) || \
-//   (CGALPY_KERNEL == CGALPY_KERNEL_FILTERED_SIMPLE_CARTESIAN_DOUBLE) || \
-//   (CGALPY_KERNEL_D == CGALPY_KERNEL_D_EPIC_D) || \
-//   (CGALPY_KERNEL_D == CGALPY_KERNEL_D_CARTESIAN_D_DOUBLE)
-// template <typename Point>
-// size_t hash_rational_point(Point& p)
-// { return boost::hash_range(p.cartesian_begin(), p.cartesian_end()); }
-// #endif
 
 #endif
