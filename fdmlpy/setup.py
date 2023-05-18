cc9dc1d6b302e6f73223d5e517b3502bb554524d
Efi Fogel <efifogel@gmail.com>
2023-05-18 17:45:48 +0300

    Changed package from fdmlpy to fdml

--- fdmlpy/setup.py
+++ fdmlpy/setup.py
@@ -1,14 +1,12 @@
 from distutils.core import setup
-package_data = {'fdmlpy': ['__init__.py', '@FDMLPY_TARGET_LINKER_FILE@', 'fdmlpy.pyi']}
-setup(# name='fdmlpy',
-      version='@CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@',
+package_data = {'fdml': ['__init__.py', '@FDMLPY_TARGET_LINKER_FILE@', 'fdmlpy.pyi']}
+setup(version='@CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@',
       description='Few Distance Measurements Robot Localization Library',
-      # py_modules=['fdmlpy'],
-      packages=['fdmlpy'],
+      packages=['fdml'],
       platforms=['any'],
       author='Barak Ugav, Efi Fogel',
       author_email='barakugav@gmail.com',
       url='https://github.com/barakugav/FDML',
-      package_dir={'fdmlpy': '.'},
+      package_dir={'fdml': '.'},
       package_data=package_data,
       license='')
