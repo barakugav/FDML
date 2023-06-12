3c0f9e5e5f8f47165189a62330d64615cad30a8f
Efi Fogel <efifogel@gmail.com>
2023-06-12 16:14:47 +0300

    Fixed setup and wheel file name & generation

--- fdmlpy/setup.py
+++ fdmlpy/setup.py
@@ -1,12 +1,16 @@
-from distutils.core import setup
-package_data = {'fdml': ['__init__.py', '@FDMLPY_TARGET_LINKER_FILE@', 'fdmlpy.pyi']}
-setup(version='@CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@',
+import distutils
+from distutils.core import Extension, setup
+package_data = {'@FDMLPY_PACKAGE_NAME@': ['__init__.py', '@FDMLPY_TARGET_LINKER_FILE@', '@FDML_TARGET_LINKER_FILE@', 'fdmlpy.pyi']}
+setup(version='@CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@.@CMAKE_PROJECT_VERSION_PATCH@',
       description='Few Distance Measurements Robot Localization Library',
-      packages=['fdml'],
-      platforms=['any'],
+      packages=['@FDMLPY_PACKAGE_NAME@'],
+      # platforms=['any'],
+      platforms=[distutils.util.get_platform().replace('-','_').replace('.','_')],
       author='Barak Ugav, Efi Fogel',
       author_email='barakugav@gmail.com',
       url='https://github.com/barakugav/FDML',
-      package_dir={'fdml': '.'},
+      package_dir={'@FDMLPY_PACKAGE_NAME@': '.'},
       package_data=package_data,
-      license='')
+      license='proprietary',
+      # NOTE: Create fake extension so that distutils builds a platform specific whl.
+      ext_modules=[Extension('fake', sources=[])])
