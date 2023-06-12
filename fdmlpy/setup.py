60135105b8d46bea86c390b8c9e283bded8b3b14
Barak Ugav <barakugav@gmail.com>
2023-06-12 17:43:09 +0300

    Build: use setuptools, force wheel to be non-pure

--- fdmlpy/setup.py
+++ fdmlpy/setup.py
@@ -1,16 +1,34 @@
-import distutils
-from distutils.core import Extension, setup
-package_data = {'@FDMLPY_PACKAGE_NAME@': ['__init__.py', '@FDMLPY_TARGET_LINKER_FILE@', '@FDML_TARGET_LINKER_FILE@', 'fdmlpy.pyi']}
+import setuptools
+import sysconfig
+from setuptools import Extension, setup
+
+
+package_data = {'@FDMLPY_PACKAGE_NAME@':
+                ['__init__.py',
+                 '@FDMLPY_TARGET_LINKER_FILE@',
+                 '@FDML_TARGET_LINKER_FILE@',
+                 'fdmlpy.pyi']}
+
+# Use a custom bdist_wheel to force non-pure wheel file
+# aka no 'pyX-none-any'
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
+
 setup(version='@CMAKE_PROJECT_VERSION_MAJOR@.@CMAKE_PROJECT_VERSION_MINOR@.@CMAKE_PROJECT_VERSION_PATCH@',
       description='Few Distance Measurements Robot Localization Library',
       packages=['@FDMLPY_PACKAGE_NAME@'],
       # platforms=['any'],
-      platforms=[distutils.util.get_platform().replace('-','_').replace('.','_')],
+      platforms=[sysconfig.get_platform().replace('-', '_').replace('.', '_')],
       author='Barak Ugav, Efi Fogel',
       author_email='barakugav@gmail.com',
       url='https://github.com/barakugav/FDML',
       package_dir={'@FDMLPY_PACKAGE_NAME@': '.'},
       package_data=package_data,
       license='proprietary',
-      # NOTE: Create fake extension so that distutils builds a platform specific whl.
-      ext_modules=[Extension('fake', sources=[])])
+      cmdclass={'bdist_wheel': bdist_wheel})
